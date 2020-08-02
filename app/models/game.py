d97691bbef13472ad28c45f0659c3f351cf8bbbd
Duncan Ryan <duncancryan1@gmail.com>
2020-08-02 14:38:51 +0100

    determine winner function written, not pretty but will get the job done for now

--- app/models/game.py
+++ app/models/game.py
@@ -8,5 +8,17 @@ class Game():
     def determine_winner(self):
         if self.player_1.choice == self.player_2.choice:
             return None
+        elif self.player_1.choice == "rock" and self.player_2.choice == "scissors":
+            return self.player_1
+        elif self.player_1.choice == "scissors" and self.player_2.choice == "paper":
+            return self.player_1
+        elif self.player_1.choice == "paper" and self.player_2.choice == "rock":
+            return self.player_1        
+        elif self.player_2.choice == "rock" and self.player_1.choice == "scissors":
+            return self.player_2
+        elif self.player_2.choice == "scissors" and self.player_1.choice == "paper":
+            return self.player_2
+        elif self.player_2.choice == "paper" and self.player_1.choice == "rock":
+            return self.player_2
         
     # logic needed which accesses player choice and determines winner, can I write game logic elsewhere???
