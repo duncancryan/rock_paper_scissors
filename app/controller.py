c2325381c79d33fb4dd31c6cfe8aecd87ca026e1
Duncan Ryan <duncancryan1@gmail.com>
2020-08-02 17:17:46 +0100

    realised I need to create another template to display the result through, also added submit input which I forgot, now just need to figure out routing

--- app/controller.py
+++ app/controller.py
@@ -9,8 +9,9 @@ def index():
 
 @app.route('/play-game', methods=['POST'])
 def play_game():
+    pass
     player_1 = Player(request.form['player-1'], request.form['player-1-choice'])
     player_2 = Player(request.form['player-2'], request.form['player-2-choice'])
-    game = Game(player_1, player_2)
-    result = game.determine_winner()
-    return render_template('index.html', title='Home', result=result)
+    new_game = Game(player_1, player_2)
+    result = new_game.determine_winner()
+    return render_template('index.html', title='Home')
