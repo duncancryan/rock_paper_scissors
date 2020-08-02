27ad81261f62f87adf0ae29423cd5d48da51dded
Duncan Ryan <duncancryan1@gmail.com>
2020-08-02 22:12:29 +0100

    adding nav bar into base template header for welcome page anchor

--- app/controller.py
+++ app/controller.py
@@ -9,7 +9,6 @@ def index():
 
 @app.route('/play-game', methods=['POST'])
 def play_game():
-    pass
     player_1 = Player(request.form['player-1'], request.form['player-1-choice'])
     player_2 = Player(request.form['player-2'], request.form['player-2-choice'])
     new_game = Game(player_1, player_2)
