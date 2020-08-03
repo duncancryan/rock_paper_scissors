35e4d398c340b3c037ceeefff236c8eea24df6fe
Duncan Ryan <duncancryan1@gmail.com>
2020-08-03 01:31:50 +0100

    linter was being pedantic about import statement so modified it to make it happy

--- app/controller.py
+++ app/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app import app
-from app.models.game import *
+from app.models.game import Game, generate_ai
 from app.models.player import *
 
 @app.route('/')
